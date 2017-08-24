0e1ac9ebb3a87e9124a964240be55abc6c8be084
rafael238 <se56160422@gmail.com>
2017-08-24 15:46:20 +0700

    branch feature

--- app/src/main/java/com/example/rafael238/myfunctionextension/MainActivity.kt
+++ app/src/main/java/com/example/rafael238/myfunctionextension/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        plus()
+
     }
+    fun plus()  = 5+5
 }
