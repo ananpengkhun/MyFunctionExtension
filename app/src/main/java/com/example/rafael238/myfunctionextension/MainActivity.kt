3823fd02a4072fbcec427888618eac4c49393dfb
rafael238 <se56160422@gmail.com>
2017-08-24 15:40:08 +0700

    branch develop

--- app/src/main/java/com/example/rafael238/myfunctionextension/MainActivity.kt
+++ app/src/main/java/com/example/rafael238/myfunctionextension/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.rafael238.myfunctionextension
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        tv_date.text = date.format()
+    }
+
+    fun Date.format() : String{
+        val sdf = SimpleDateFormat("yyyy-MM-dd")
+        return sdf.format(this)
     }
 }
